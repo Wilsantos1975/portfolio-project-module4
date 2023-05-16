188654b6acf959307339e585cacd1b57e019f98b
Wilsantos1975 <Faustosantos@pursuit.org>
2023-05-16 19:03:55 -0400

    added sql table and seeded the db

---
+++ back-end/db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS records;
+
+CREATE DATABASE records;
+
+\c records;
+
+CREATE TABLE records (
+    id SERIAL PRIMARY KEY,
+    album TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    condition TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    image TEXT NOT NULL
+)
---
+++ back-end/db/seed.sql
@@ -0,0 +1,17 @@
+\c records;
+
+
+INSERT INTO records (album, artist, year, condition, price, image) VALUES 
+('The Dark Side of the Moon', 'Pink Floyd', 1973, 'Mint', 100, 'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png'),
+('All of this life', 'The Record Company', 2018, 'Used', 35,'https://upload.wikimedia.org/wikipedia/en/b/b6/All_of_This_Life.jpg'),
+('Abraxas', 'Carlos Santana', 1970, 'Used', 50, 'https://upload.wikimedia.org/wikipedia/en/9/9d/Abraxas.jpg'),
+('The Wall', 'Pink Floyd', 1979, 'Mint', 100, 'https://upload.wikimedia.org/wikipedia/en/3/3a/PinkFloyd-Animals-Frontal.jpg'),
+('The Beatles', 'The Beatles', 1968, 'Used', 50, 'https://upload.wikimedia.org/wikipedia/en/4/4b/Beatles_-_White_Album.jpg'),
+('AC-DC', 'Back In Black', 1980, 'Mint', 75, 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/ACDC_Back_in_Black_cover.svg/1920px-ACDC_Back_in_Black_cover.svg.png'),
+('Signature Collection', 'B.B. King', 2014, 'Used', 68, 'https://merchbar.imgix.net/product/vinylized/upc/64/8712177064441.jpg?quality=60&auto=compress,format&w=1200'),
+('The Best of', 'The Doors', 1985, 'Used', 45, 'https://upload.wikimedia.org/wikipedia/en/4/4d/Doors_-_Best_of.jpg'),
+('The Best of', 'The Rolling Stones', 1971, 'Used', 45, 'https://upload.wikimedia.org/wikipedia/en/4/4d/Doors_-_Best_of.jpg'),
+('The Best of', 'The Beatles', 1968, 'Used', 45, 'https://upload.wikimedia.org/wikipedia/en/4/4d/Doors_-_Best_of.jpg'),
+('Outlandos Damour', 'The Police', 1978, 'Mint', 50, 'https://upload.wikimedia.org/wikipedia/en/f/f3/Police-album-outlandosdamour.jpg'),
+('Brothers', 'The Black Keys', 2010, 'Mint', 45, 'https://upload.wikimedia.org/wikipedia/en/9/93/The_Black_Keys_-_Brothers.jpg');
+
